1a8adda9bc95214dd0b7cd056f52dc3cd5e2f07c
ZohaibYar <zohaibyar@gmail.com>
2025-03-17 16:03:46 +0000

    main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -82,7 +82,7 @@ resource "aws_security_group" "web_sg" {
 # EC2 Instance
 # =========================
 resource "aws_instance" "web_server" {
-  ami                    = " ami-08b5b3a93ed654d19"  
+  ami                    = "ami-08b5b3a93ed654d19"  
   instance_type          = "t2.micro"
   subnet_id              = aws_subnet.public_subnet_1.id
   vpc_security_group_ids = [aws_security_group.web_sg.id]
