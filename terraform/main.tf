68f12bb60104a38a2987d7b15b072bc777ce7e29
ZohaibYar <zohaibyar@gmail.com>
2025-03-17 16:21:49 +0000

    main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,4 +1,11 @@
-#starthere
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"  # Specify a compatible version
+    }
+  }
+}
 provider "aws" {
   region = "us-east-1"
 }
